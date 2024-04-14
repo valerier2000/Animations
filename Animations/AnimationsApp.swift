ed7416f31a619da62e6b6a9c4fdb6498856d5aa7
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-04-14 09:07:57 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Valeriia Rohatynska on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
